c0dcef8c3f5da14b1bb725faebf2c6a34fe0a28e
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-17 14:08:27 -0400

    Fix indentation

--- src/WmiScripting/config/connections.php
+++ src/WmiScripting/config/connections.php
@@ -1,17 +1,17 @@
 <?php
 
 return [
-  'default' => 'local',
+    'default' => 'local',
 
-  'servers' => [
-      'local' => [
-          'server'          => '.',
-          'namespace'       => 'Root\CIMv2',
-          'user'            => null,
-          'password'        => null,
-          'locale'          => null,
-          'authority'       => null,
-          'security_flags'  => null,
-      ],
-  ],
+    'servers' => [
+          'local' => [
+              'server'          => '.',
+              'namespace'       => 'Root\CIMv2',
+              'user'            => null,
+              'password'        => null,
+              'locale'          => null,
+              'authority'       => null,
+              'security_flags'  => null,
+          ],
+    ],
 ];
