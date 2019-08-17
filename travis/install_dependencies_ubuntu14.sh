76cb0c73ca424db4b9b2516e31d350479feb672a
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-17 09:50:28 -0400

    Working with Travis

--- travis/install_dependencies_ubuntu14.sh
+++
@@ -1,8 +0,0 @@
-# Borrowed from Peachpie
-
-# Install .NET Core # https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-current
-
-# Install .NET Core SDK
-sudo apt-get install apt-transport-https
-sudo apt-get update
-sudo apt-get install dotnet-sdk-2.1 -y
