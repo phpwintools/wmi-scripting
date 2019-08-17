f030f24067fb1e1124e4746d62b719a109c76263
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-16 22:51:39 -0400

    Remove unneccessary code

--- src/WmiScripting/Support/ApiObjects/Contracts/Locator.php
+++ src/WmiScripting/Support/ApiObjects/Contracts/Locator.php
@@ -6,7 +6,5 @@ use PhpWinTools\WmiScripting\Connection;
 
 interface Locator extends WbemObject
 {
-    public static function connectLocal(): Services;
-
     public function connectServer(Connection $connection = null): Services;
 }
