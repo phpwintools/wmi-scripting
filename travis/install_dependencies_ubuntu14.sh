856c2440faa0cffb8668859acebc76c36d5479c2
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-17 09:48:46 -0400

    Working with Travis

--- travis/install_dependencies_ubuntu14.sh
+++ travis/install_dependencies_ubuntu14.sh
@@ -2,10 +2,6 @@
 
 # Install .NET Core # https://www.microsoft.com/net/download/linux-package-manager/ubuntu14-04/sdk-current
 
-# Register Microsoft key and feed
-wget -q packages-microsoft-prod.deb https://packages.microsoft.com/config/ubuntu/14.04/packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-
 # Install .NET Core SDK
 sudo apt-get install apt-transport-https
 sudo apt-get update
