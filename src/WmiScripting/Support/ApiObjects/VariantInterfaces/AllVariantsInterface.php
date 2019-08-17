2468da9f269ec060a2e1ae444e511075062756db
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-08-17 16:57:59 -0400

    Add AllVariantsInterface

---
+++ src/WmiScripting/Support/ApiObjects/VariantInterfaces/AllVariantsInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace PhpWinTools\WmiScripting\Support\ApiObjects\VariantInterfaces;
+
+interface AllVariantsInterface extends
+    LocatorVariant,
+    ObjectVariant,
+    ObjectExVariant,
+    ObjectPathVariant,
+    ObjectSetVariant,
+    PropertySetVariant,
+    PropertyVariant,
+    QualifierSetVariant,
+    QualifierVariant,
+    ServicesVariant
+{
+}
