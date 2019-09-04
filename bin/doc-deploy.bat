9eca9f25f6f32189aff6ecd01db9acd35677f503
Joe Springe <518362+jspringe@users.noreply.github.com>
2019-09-04 13:47:45 -0400

    Remove deploy scripts

--- bin/doc-deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-yarn docs:build
-
-cd docs\.vuepress\dist
-
-git init && git add -A &&  git commit -m "deploy" && git push -f https://github.com/phpwintools/wmi-scripting.git master:gh-pages
-
-cd ..\..\..
--- bin/doc-deploy.sh
+++
@@ -1,3 +0,0 @@
-yarn docs:build && cd docs/.vuepress/dist && git init &&  git add -A && git commit -m "deploy" && git push -f https://github.com/phpwintools/wmi-scripting.git master:gh-pages
-
-cd -
